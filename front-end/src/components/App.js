1a1026521820649d1ded473be77a6295a4a81e4a
Henrique Herbs Rosa <henriquehhr@gmail.com>
2022-08-21 02:04:51 -0300

    fixed front-end port

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,22 +3,26 @@ import axios from "axios";
 
 function App() {
   const [student, setStudent] = useState(null);
-    
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if(!student){
-        alert("Putz! Não há estudantes cadastrados para o sorteio!");
-      } else {
-        setStudent(student);
+
+  useEffect(() => {
+    async function fetchData() {
+      const API_URL = process.env.REACT_APP_BASE_URL;
+      try {
+        const response = await axios.get(`${API_URL}/students/random`);
+        const student = response.data;
+        if (!student) {
+          alert("Putz! Não há estudantes cadastrados para o sorteio!");
+        } else {
+          setStudent(student);
+        }
+      } catch (error) {
+        console.log(error);
+        alert("Não foi possível realizar o sorteio!");
       }
-    } catch (error) {
-      alert("Não foi possível realizar o sorteio!");
     }
+    fetchData();
   }, []);
-  
+  console.log("teste");
   return (
     student ? <h1>{student.name}</h1> : "Carregando..."
   )
